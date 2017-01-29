2078bb1424f754bf6bc567e0622776c4bd209534
padster <padsterpat@gmail.com>
2017-01-29 12:38:26 -0800

    Temporary sequential user number graphic

--- run.py
+++ run.py
@@ -14,17 +14,16 @@ SEQUENTIAL_USERS = []
 # Add logic here to determine which users we want to include or exclude:
 def filterUser(user):
     if user['isstaff'] == 'TRUE':
-
         return False
     # preMOOC = user['entryTest']['In how many MOOCs have you participated at least partially?']
     # return preMOOC in ['1-2.', '3-7.', '8 or more.']
 
     # return 'prepost' in user
     # return 'exitTest' in user
-    # return user['nforum_posts'] != "NA"
+    return user['nforum_posts'] != "NA"
     # return user['grade'] > 0.5
     # return user['education'] == 'high_school'
-    return True
+    # return True
 
 def filterUsers(users):
     return {userID for userID in users if userID in files.USER_ROWS_BY_ID and filterUser(files.USER_ROWS_BY_ID[userID])}
@@ -36,9 +35,9 @@ def printElement(element, cat, stayUsers, leaveUsers, padding):
     leaveCount = len(leaveUsers)
     leaveGrade = files.averageUserGrade(leaveUsers)
     if cat == '|':
+        print ('%s (%s) %s - %d users, %d left (%.2f)') % (padding, cat, element['name'], userCount, leaveCount, leaveGrade)
         SEQUENTIAL_USERS.append(userCount)
     # if leaveCount > 0:
-    print ('%s (%s) %s - %d users, %d left (%.2f)') % (padding, cat, element['name'], userCount, leaveCount, leaveGrade)
 
 
 # Useful representations of category types
